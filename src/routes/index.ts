97869ea51bf8b204a3f2760181445cb40f2e2aec
Rifatul Islam <rifat.islam.cse@gmail.com>
2025-12-21 17:05:41 +0600

    feat: supplier and purchase order

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,14 @@ import authRoutes from "./auth.routes";
 import categorieRoutes from "./categories.routes"
 import itemsRoutes from "./items.routes"
 import customersRoutes from "./customers.routes"
+import supplierRoutes from "./suppliers.routes"
 const router=Router()
 
 router.use('/auth',authRoutes)
-router.use("/categories",categorieRoutes);
+router.use("/categories",categorieRoutes)
 router.use("/items", itemsRoutes)
 router.use("/customers", customersRoutes)
+router.use("/supplier", supplierRoutes)
+
 
 export default router;
